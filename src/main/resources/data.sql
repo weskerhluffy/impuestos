e444034db3560a78750bd21bc22428617f60529f
ernesto alvarado <ernesto.alvarado@adroll.com>
2019-04-14 09:37:32 -0500

    pruebas h2 func

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into factura_vigente values (null, 2019, '2019-01-01', 'folio1', 1, 10, 0, 0, 'rfc1');
+insert into factura_vigente values (null, 2019, '2019-01-02', 'folio2', 1, 10, 0, 0, 'rfc2');
+insert into factura_vigente values (null, 2019, '2019-01-03', 'folio3', 1, 10, 0, 0, 'rfc3');
--- src/test/java/org/nada/dao/FacturaVigenteDAOTests.java
+++ src/test/java/org/nada/dao/FacturaVigenteDAOTests.java
@@ -1,7 +1,13 @@
 package org.nada.dao;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import java.util.stream.StreamSupport;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.nada.models.FacturaVigente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -9,6 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+// XXX: https://stackoverflow.com/questions/39690094/spring-boot-default-profile-for-integration-tests
 @ActiveProfiles("test")
 public class FacturaVigenteDAOTests {
 
@@ -18,5 +25,11 @@ public class FacturaVigenteDAOTests {
 	@Test
 	public void pruebaCustomizacion() {
 		facturaVigenteDAO.metodoPrueba();
+	}
+
+	@Test
+	public void pruebaCargaTodo() {
+		Iterable<FacturaVigente> todos = facturaVigenteDAO.findAll();
+		assertTrue(StreamSupport.stream(todos.spliterator(), false).count() > 0);
 	}
 }
